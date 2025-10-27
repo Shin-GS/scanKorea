295536a6c7ca1eb139b9f55315b90d6f48789bd7
DESKTOP-T72GEF9\user <rudtjq0842@naver.com>
2025-10-27 13:35:22 +0900

    feat: GlobalExceptionHandler 분리
    API를 위한 GlobalApiExceptionHandler 추가
    페이지 요청을 위한 GlobalExceptionHandler 유지
    우선순위 지정

---
+++ src/main/java/com/scankorea/server/common/exception/GlobalApiExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.scankorea.server.common.exception;
+
+import com.scankorea.server.common.code.ErrorCode;
+import com.scankorea.server.common.response.CustomResponseBuilder;
+import com.scankorea.server.common.response.Response;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(annotations = org.springframework.web.bind.annotation.RestController.class)
+@org.springframework.core.annotation.Order
+@RequiredArgsConstructor
+public class GlobalApiExceptionHandler {
+    private final CustomResponseBuilder responseBuilder;
+
+    @ExceptionHandler(CustomException.class)
+    public Response<Void> handleCustomException(CustomException e) {
+        log.error("handle CustomException: ", e);
+        ErrorCode errorCode = e.getCode();
+        return responseBuilder.of(errorCode);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public Response<Void> handleException(Exception e) {
+        log.error("handle exception: ", e);
+        return responseBuilder.of(ErrorCode.INTERNAL_SERVER_ERROR);
+    }
+}
--- src/main/java/com/scankorea/server/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/scankorea/server/common/exception/GlobalExceptionHandler.java
@@ -1,29 +1,16 @@
 package com.scankorea.server.common.exception;
 
-import com.scankorea.server.common.code.ErrorCode;
-import com.scankorea.server.common.response.CustomResponseBuilder;
-import com.scankorea.server.common.response.Response;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
-@RequiredArgsConstructor
+@ControllerAdvice
+@org.springframework.core.annotation.Order(org.springframework.core.Ordered.HIGHEST_PRECEDENCE)
 public class GlobalExceptionHandler {
-    private final CustomResponseBuilder responseBuilder;
-
-    @ExceptionHandler(CustomException.class)
-    public Response<Void> handleCustomException(CustomException e) {
-        log.error("handle CustomException: ", e);
-        ErrorCode errorCode = e.getCode();
-        return responseBuilder.of(errorCode);
-    }
-
     @ExceptionHandler(Exception.class)
-    public Response<Void> handleException(Exception e) {
+    public String handle(Exception e) {
         log.error("handle exception: ", e);
-        return responseBuilder.of(ErrorCode.INTERNAL_SERVER_ERROR);
+        return "/views/error";
     }
 }
