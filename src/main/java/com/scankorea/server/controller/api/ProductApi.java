b57e3eabca433eecd4e5bf79f8c46102e1b740ee
DESKTOP-T72GEF9\user <rudtjq0842@naver.com>
2025-10-30 16:37:44 +0900

    feat: API 이름 변경
    좀더 직관적으로 변경

--- src/main/java/com/scankorea/server/controller/api/ProductApi.java
+++ src/main/java/com/scankorea/server/controller/api/ProductApi.java
@@ -7,21 +7,18 @@ import com.scankorea.server.common.response.Response;
 import com.scankorea.server.service.product.ProductService;
 import com.scankorea.server.service.product.response.ProductViewResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-public class LookupApi {
+public class ProductApi {
     private final CustomResponseBuilder responseBuilder;
     private final ProductService productService;
 
-    @GetMapping("/lookup")
-    public Response<ProductViewResponse> byGtin(@RequestParam String gtin,
-                                                @RequestParam(name = "lang") LanguageCode lang) {
+    @GetMapping("/products/{gtin}")
+    public Response<ProductViewResponse> productDetail(@PathVariable(name = "gtin") String gtin,
+                                                       @RequestParam(name = "lang") LanguageCode lang) {
         ProductViewResponse productView = productService.findView(gtin, lang);
         return responseBuilder.of(SuccessCode.SUC, productView);
     }
